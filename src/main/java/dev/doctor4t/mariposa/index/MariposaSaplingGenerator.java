269935985f46e0bc0d6768fd6fdcb7030b5cf1bb
JR1811 <janreichelt77@gmail.com>
2025-02-22 20:17:51 +0100

    fixed wrong sapling tree generation

--- src/main/java/dev/doctor4t/mariposa/index/MariposaSaplingGenerator.java
+++ src/main/java/dev/doctor4t/mariposa/index/MariposaSaplingGenerator.java
@@ -1,10 +1,10 @@
 package dev.doctor4t.mariposa.index;
 
+import dev.doctor4t.mariposa.datagen.MariposaConfiguredFeatures;
 import net.minecraft.block.sapling.LargeTreeSaplingGenerator;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.TreeConfiguredFeatures;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
@@ -12,14 +12,14 @@ public interface MariposaSaplingGenerator {
     class SEQUOIA extends LargeTreeSaplingGenerator {
         @Nullable
         @Override
-        protected RegistryKey<ConfiguredFeature<?, ?>> getLargeTreeFeature(Random random) {
-            return random.nextBoolean() ? TreeConfiguredFeatures.MEGA_SPRUCE : TreeConfiguredFeatures.MEGA_PINE;
+        protected RegistryKey<ConfiguredFeature<?, ?>> getTreeFeature(Random random, boolean bees) {
+            return null;
         }
 
         @Nullable
         @Override
-        protected RegistryKey<ConfiguredFeature<?, ?>> getTreeFeature(Random random, boolean bees) {
-            return TreeConfiguredFeatures.SPRUCE;
+        protected RegistryKey<ConfiguredFeature<?, ?>> getLargeTreeFeature(Random random) {
+            return MariposaConfiguredFeatures.SEQUOIA;
         }
     }
 }
