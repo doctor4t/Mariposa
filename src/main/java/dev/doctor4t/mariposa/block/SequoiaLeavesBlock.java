0802c5b846a2535e35a876a8a7e06a643aa6685e
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-10-02 09:07:25 -0500

    update to 1.21.9

--- src/main/java/dev/doctor4t/mariposa/block/SequoiaLeavesBlock.java
+++ src/main/java/dev/doctor4t/mariposa/block/SequoiaLeavesBlock.java
@@ -1,9 +1,9 @@
 package dev.doctor4t.mariposa.block;
 
 import net.minecraft.block.TintedParticleLeavesBlock;
-import net.minecraft.particle.EntityEffectParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.particle.ParticleUtil;
+import net.minecraft.particle.TintedParticleEffect;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
@@ -15,7 +15,7 @@ public class SequoiaLeavesBlock extends TintedParticleLeavesBlock {
 
 	@Override
 	protected void spawnLeafParticle(World world, BlockPos pos, Random random) {
-		EntityEffectParticleEffect entityEffectParticleEffect = EntityEffectParticleEffect.create(ParticleTypes.TINTED_LEAVES, 0x4d6533);
-		ParticleUtil.spawnParticle(world, pos, random, entityEffectParticleEffect);
+		TintedParticleEffect tintedParticleEffect = TintedParticleEffect.create(ParticleTypes.TINTED_LEAVES, 0x4d6533);
+		ParticleUtil.spawnParticle(world, pos, random, tintedParticleEffect);
 	}
 }
