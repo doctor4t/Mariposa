1bc7e17a10d1f0b608540f1f727953c246634d3f
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-04-07 14:12:59 -0500

    Update MariposaRecipeProvider.java

--- src/main/java/dev/doctor4t/mariposa/data/provider/MariposaRecipeProvider.java
+++ src/main/java/dev/doctor4t/mariposa/data/provider/MariposaRecipeProvider.java
@@ -43,8 +43,8 @@ public class MariposaRecipeProvider extends FabricRecipeProvider {
 		};
 	}
 
-	public static void offerStackedBlockRecipe(RecipeProvider provider, RecipeOutput output, ItemLike created, ItemLike input) {
-		provider.shaped(RecipeCategory.BUILDING_BLOCKS, created, 6)
+	public static void offerStackedBlockRecipe(RecipeProvider provider, RecipeOutput output, ItemLike result, ItemLike input) {
+		provider.shaped(RecipeCategory.BUILDING_BLOCKS, result, 6)
 				.define('#', input)
 				.pattern("###")
 				.pattern("###")
